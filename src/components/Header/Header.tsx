9460b1ec005386818a60c312dbe439df4612a6f8
Jitendra Kumar <120254332+jitu097@users.noreply.github.com>
2025-02-17 16:19:18 +0530

    resume update
    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Container } from './styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { NavHashLink, HashLink } from 'react-router-hash-link'
 import { useState } from 'react'
-import Resume from '../../assets/JITENDRA_RESUME.pdf'
+import Resume from '../../assets/JITENDRA KUMAR_RESUME.pdf'
 export function Header() {
   const [isActive, setActive] = useState(false)
   function toggleTheme() {
